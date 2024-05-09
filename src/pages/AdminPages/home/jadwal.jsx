b6afe91525f8a5dd646fe6f92a1464103ce5c866
Sigit Surya <sigitsurya23@gmail.com>
2024-05-09 13:41:42 +0700

    change url

--- src/pages/AdminPages/home/jadwal.jsx
+++ src/pages/AdminPages/home/jadwal.jsx
@@ -25,19 +25,7 @@ const JadwalHome = () => {
 
     return(
         <>
-            <div className="carousel carousel-center max-w-full p-4 space-x-4 rounded-box">
-                {dates.map((date, index) => (
-                    <div key={index} className={`max-content carousel-item ${currentDate == date.day ? 'bg-secondary' : 'bg-primary'} text-center rounded-full cursor-pointer hover:bg-primary-content hover:text-white`}>
-                        <div className="-rotate-90 h-full pt-12 pb-12">
-                            <span className="font-semibold text-nowrap text-2xl">{date.day} {date.month}</span>
-                        </div>
-                    </div>
-                ))}
-            </div>
-
-            <div className="my-8 w-full bg-base-300 p-4 h-3/4 rounded-lg">
-                test
-            </div>
+            
         </>
     )
 }
